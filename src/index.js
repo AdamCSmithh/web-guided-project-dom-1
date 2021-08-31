977e36c564993c837f5ffc2963a7f487429b3d9f
acshatr <46507461+acshatr@users.noreply.github.com>
2021-08-30 23:46:53 -0700

    adam-smith

--- src/index.js
+++ src/index.js
@@ -3,52 +3,73 @@
 //  Newer: querySelector, querySelectorAll
 //  Select the following single elements from the div.card
 
+const caseyNav =document.getElementById('mainNav');
+const aTag = document.getElementsByTagName('a');
+const menuItem = document.getElementsByClassName('menu-item')
+
 // A- finding across the entire DOM
-const header = null
-const logoTitle = null
-const firstCard = null
+const header = document.querySelector('header');
+const logoTitle = document.querySelector('#logoTitle');
+const firstCard = document.querySelector('.card:nth-of-type(1)');
 // B- finding within one particular element
-const imageFirstCard = null
-const titleFirstCard = null
-const subtitleFirstCard = null
-const textFirstCard = null
+const imageFirstCard = firstCard.querySelector('img');
+const titleFirstCard = firstCard.querySelector('h2');
+const subtitleFirstCard = firstCard.querySelector('.card-subtitle');
+const textFirstCard = firstCard.querySelector('.card-text');
 // C- traversing with dot notation
-const link1FirstCard = null
-const link2FirstCard = null
+const link1FirstCard = textFirstCard.nextElementSibling;
+const link2FirstCard = link1FirstCard.nextElementSibling;
 
 
 // 👉 2- Finding collections of elements in the DOM
 // A- Find all the anchor tags inside the nav element
+const links = document.querySelectorAll('nav a');
+
 // B- Loop over the links and console.log their text content
+links.forEach(link => console.log(link.textContent));
 // C- Turn the collection of links into a real array
+const linksArray = Array.from(links);
+const foundLink = linksArray.find(link => link.textContent === 'Home');
 // D- Use .filter to find the anchor tag with the textContent of "Home"
 
 
 // 👉 3- Changing an element's text content
 //  A- Change the cat-related content into dog-related content
+logoTitle.textContent = "Lambda Dog";
+titleFirstCard.textContent = "Dog or Doge"; 
 //  B- Have the students research online the difference between textContent and innerText
 
 
 // 👉 4- Changing any property
 //  A- Using dot notation to change a few attributes
+logoTitle.className = 'logo heading banana';
 //  B- Using .setAttribute to change a few attributes
+link1FirstCard.setAttribute('href', 'https://www.doggoipsum.com/');
 
 
 // 👉 5- Changing the styling of an element
 //  A- By changing the class names on the element with the classList API
+header.classList.add('sky');
+header.classList.remove('sky');
 //  B- By manipulating inline styles on the element
-
+header.style.fontSize = '2em';
 
 // 👉 6- Creating new elements from scratch and appending them
 // Create a new link inside the nav for "Blog"
+const blogLink = document.createElement('a');
+blogLink.textContent = "Blog";
+blogLink.href = '#';
+document.querySelector('nav').appendChild(blogLink);
 
-
+blogLinks.classList.add('menu-item');
 // 👉 7- Making a copy of the card and appending it to the card group
 // DOM nodes can only exist in one spot in the DOM
 // We cannot append the same copy multiple times
+const secondCard = firstCard.cloneNode(true);
+document.querySelector('.card-group').appendChild(seecondCard);
 
 
 // 👉 8- Removing an existing element and putting it back [STRETCH if time allows]
-
+header.remove();
 
 // 👉 9- Show to students the insertAdjacentHTML method [STRETCH if time allows]
